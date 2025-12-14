6048a0ad5fc8506ce1aa30836d7918c45663e0d0
Sandeep Mishra <sandeepmsr123@gmail.com>
2025-12-15 03:08:57 +0530

    connected DB

---
+++ src/index.js
@@ -0,0 +1,35 @@
+// require("dotenv").config();  another syntax for importing dotenv
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({             
+    path : "./.env"
+})
+
+
+connectDB()
+
+
+
+// import mongoose from "mongoose";
+// import express from "express";
+// import { DB_NAME } from "./constants";
+// (
+//     async() =>{
+//         const app = express();
+//         try{
+//             await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//             app.on( "error ", (error)=>{
+//                 console.log("ERROR",error);
+//                 throw error;
+//             })
+
+//             app.listen("process.env.PORT" , ()=>{
+//                 console.log(`Server is running on ${process.env.PORT}`)
+//             })
+//         }catch(error){
+//             console.log("ERROR",error);
+//             throw error;
+//         }
+//     }
+// )()
