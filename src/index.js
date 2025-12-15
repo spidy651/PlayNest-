b94a6cfcdc7c41428674cc4ba05dd9879e9da9c9
Sandeep Mishra <sandeepmsr123@gmail.com>
2025-12-15 16:36:52 +0530

    Utils + appjs Configuration with express

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 // require("dotenv").config();  another syntax for importing dotenv
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import app from "./app.js";
 dotenv.config({             
     path : "./.env"
 })
 
 
-connectDB()
-
+connectDB().then(()=>{
+    app.on( "error ", (error)=>{
+                console.log("ERROR",error);
+                throw error;
+            })
+    app.listen(process.env.PORT, ()=>{
+        console.log(`Server is running on PORT : ${process.env.PORT}`);
+    })
+})
+    .catch((error)=>{
+        console.log("Server startup error",error);
+    })
 
 
 // import mongoose from "mongoose";
