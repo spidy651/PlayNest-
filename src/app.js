cb0a0c258a880d05ba3bf15026140b99764930ea
Sandeep Mishra <sandeepmsr123@gmail.com>
2025-12-19 23:56:00 +0530

    declared routes

--- src/app.js
+++ src/app.js
@@ -13,4 +13,9 @@ app.use(express.static("public"));
 
 app.use(cookieParser());
 
-export default app;
+//routes import
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users" , userRouter)      //we used /api/v1 just for the info that we are creating api and might make changes in future so v1
+export default app;                       //we used use instead of get because we are not using routes directly we are using middleware for that 
